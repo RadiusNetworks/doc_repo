0d2d73313c20e4371c1487d8b35a1b1b090de638
Aaron Kromer <aaron.kromer@gmail.com>
2017-06-19 12:24:17 -0400

    Minor refactor to improve intent expression
    Make it a little more explicit what is happening with the inclusion
    check. This also ensures we perform a comparison against the lower case
    characters. While this is unlikely an issue it does improve support for
    the rare case we get a "JPG" or "TXT" type extension.

--- lib/doc_repo/repository.rb
+++ lib/doc_repo/repository.rb
@@ -1,14 +1,23 @@
 module DocRepo
   class Repository
+    REDIRECT_FORMATS = %w[
+      .jpg
+      .png
+      .jpeg
+      .svg
+      .css
+      .txt
+    ]
+
     def respond(slug, &block)
-      if %w(.jpg .png .jpeg .svg .css .txt).include? File.extname(slug)
+      if REDIRECT_FORMATS.include?(File.extname(slug).downcase)
         yield DocRepo::Response.redirect(get_redirect_url(slug))
       else
         yield DocRepo::Response.html(render_page(slug))
       end
     end
 
-    private
+  private
 
     def render_page(slug)
       DocRepo::Page.new(slug).to_html
